38eb19d70f9e2384698113b52ca7c1b04d6efb71
dsissoko <dsissoko@users.noreply.github.com>
2026-06-05 15:43:49 +0000

    Deploying to gh-pages from @ dsissoko/oneticket-core@5be3b64d221d83fe453542141a268ba8d2ffd55c 🚀

---
+++ monjournal/app/assets/chevron-up-byAd61VE.js
@@ -0,0 +1,7 @@
+import{l as o}from"./index-GcenV0e2.js";
+/**
+ * @license lucide-react v1.17.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=o("chevron-down",[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]]),e=o("chevron-up",[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]]);export{a as C,e as a};
